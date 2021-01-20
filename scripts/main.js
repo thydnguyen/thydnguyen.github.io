2883b4abbfcafbf3bfabda8bd012471fa406910a
thydnguyen@gmail.com <thydnguyen@gmail.com>
2021-01-20 16:01:57 -0500

    Javascript added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-// Put your JS here.
+function myFunction() {
+  document.getElementById('download').href = 'https://www.google.com/chrome/';
+  document.getElementById('image').src = 'images/chrome.png';
+}
